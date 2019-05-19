31684718df54a13fad1a860e5b1aa23e296fcbc6
Richard <richardoosterlaken@outlook.com>
2019-05-19 19:32:02 +0200

    Init.

---
+++ src/containers/Site.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+function Site() {
+    return (
+        <div>
+            <h1>Site.js</h1>
+            <p>Inside Site.js.</p>
+            <Button>Fake Semantic-UI Button</Button>
+        </div>
+    );
+}
+
+export default Site;

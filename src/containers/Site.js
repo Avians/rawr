cf776458995f773354714b790b547bc142bee761
notzain <zain.x.ahmad@gmail.com>
2019-05-19 19:44:48 +0200

    Center page

--- src/containers/Site.js
+++ src/containers/Site.js
@@ -1,13 +1,22 @@
+import { Button, Container } from "semantic-ui-react";
+
 import React from "react";
-import { Button } from "semantic-ui-react";
 
 function Site() {
     return (
-        <div>
+        <Container textAlign="center">
             <h1>Site.js</h1>
             <p>Inside Site.js.</p>
-            <Button>Fake Semantic-UI Button</Button>
-        </div>
+            <Button
+                content="Magic button"
+                icon="earlybirds"
+                labelPosition="left"
+                onClick={e => {
+                    e.preventDefault();
+                    alert("Why do you hurt me like this?");
+                }}
+            />
+        </Container>
     );
 }
 
